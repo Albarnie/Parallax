36b5dde1176eae1c527713b11990844be1d37e75
Albarnie <albertedgelltaylor@gmail.com>
2020-08-23 13:08:43 +1000

    Added code

---
+++ ParallaxElement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Albarnie.Parallax
+{
+    public class ParallaxElement : MonoBehaviour
+    {
+        bool hasPosition;
+        public Vector2 originalPosition;
+
+        private void OnEnable()
+        {
+            if (!hasPosition)
+            {
+                originalPosition = transform.position;
+                hasPosition = true;
+            }
+
+            if (ParallaxManager.manager != null)
+            {
+
+                //Offset the object to preserve its position
+                Vector2 delta = originalPosition - (Vector2)ParallaxManager.manager.transform.position;
+                transform.position -= (Vector3)(delta * ParallaxManager.manager.axis) * (transform.position.z / ParallaxManager.manager.maxDistance);
+
+                //Add the object to the list of objects that should be parallaxed
+
+                ParallaxManager.manager.elements.Add(this);
+            }
+        }
+
+        private void OnDisable()
+        {
+            //Remove the object from the list
+            if (ParallaxManager.manager != null)
+            {
+                ParallaxManager.manager.elements.Remove(this);
+            }
+        }
+    }
+}
---
+++ ParallaxManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Albarnie.Parallax
+{
+    public class ParallaxManager : MonoBehaviour
+    {
+        public static ParallaxManager manager;
+
+        public float maxDistance = 5000;
+
+        public Vector2 axis = Vector2.one;
+
+        public Vector2 delta { get; private set; }
+        public List<ParallaxElement> elements;
+
+        Transform cam;
+        Vector2 lastCameraPos;
+
+        private void Awake()
+        {
+            //Singleton
+            if (manager == null)
+            {
+                //DontDestroyOnLoad(gameObject);
+                manager = this;
+            }
+            else if (manager != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void Start()
+        {
+            //Get a reference to the camera
+            cam = Camera.main.transform;
+
+            lastCameraPos = cam.position;
+        }
+
+        private void Update()
+        {
+            //Get the delta from the last frame
+            delta = (Vector2)cam.position - lastCameraPos;
+
+            //If the camera has moved since the last frame
+            if (delta.sqrMagnitude > 0)
+            {
+                UpdateParallax();
+            }
+            lastCameraPos = cam.position;
+        }
+
+        void UpdateParallax()
+        {
+            foreach (ParallaxElement element in elements)
+            {
+                element.transform.position += (Vector3)(delta * axis) * (element.transform.position.z / maxDistance);
+            }
+        }
+    }
+}
